f73d3860ac8fd574ce772827dadf84dfd11c720c
batitous <bburles@gmail.com>
2013-05-22 18:24:57 +0200

    Add LICENSE.txt and update license

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ dvos
 ====
 
 A tiny operating system for the NXP Cortex M3 micro-controller.
+
+See LICENSE.txt for license of this code. 
--- dvos/hw/include/drivers/mem-protocol.h
+++ dvos/hw/include/drivers/mem-protocol.h
@@ -1,4 +1,21 @@
+/*
+	Dvos - Copyright (C) 2013 Baptiste Burles, Christophe Casson, Sylvain Fay-Chatelard
 
+    This file is part of Dvos.
+
+    Dvos is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Dvos is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #ifndef LIB_HW_MEM_PROTOCOL_H
 #define LIB_HW_MEM_PROTOCOL_H
 
