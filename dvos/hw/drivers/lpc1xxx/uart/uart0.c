4ba8da15041b2f4253772286509a60b98dd5569f
batitous <bburles@gmail.com>
2013-09-04 15:54:51 +0200

    uart ?

--- dvos/hw/drivers/lpc1xxx/uart/uart0.c
+++ dvos/hw/drivers/lpc1xxx/uart/uart0.c
@@ -1,21 +1,21 @@
 /*
-	Dvos - Copyright © 2013 Baptiste Burles, Christophe Casson, Sylvain Fay-Chatelard
-
-    This file is part of Dvos.
-
-    Dvos is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 3 of the License, or
-    (at your option) any later version.
-
-    Dvos is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ Dvos - Copyright © 2013 Baptiste Burles, Christophe Casson, Sylvain Fay-Chatelard
+ 
+ This file is part of Dvos.
+ 
+ Dvos is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 3 of the License, or
+ (at your option) any later version.
+ 
+ Dvos is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include "../../kernel/include/kernel-dvos.h"
 #include "../include/libs-hardware.h"
 
@@ -59,6 +59,8 @@ void setUartIoCon(void)
 //#               define setBaudrate()    LPC_UART0->DLL = 78; LPC_UART0->DLM = 0;
 //38400
 //#       define setBaudrate()    LPC_UART0->DLL = 117; LPC_UART0->DLM = 0;
+#endif
+
 
 #ifdef MCU_IS_LPC17XX
 
@@ -95,15 +97,15 @@ static volatile KIOStream * uartStream;
 void UART0_IRQHandler(void)
 {
     UInt32 status = LPC_UART0->IIR;
-        
+    
     //Check if interrupt is pending
     if( (status & 1)==0)
     {
         status = (status >> 1) & 0x07;
-
+        
         //Receive Data Available
         if(status==0x02)
-        {   
+        {
             setByteToStream((KIOStream *)uartStream,LPC_UART0->RBR);
             irqWakeUpTaskFromStream((KIOStream *)uartStream);
         }
@@ -114,9 +116,9 @@ void UART0_IRQHandler(void)
 
 void powerOnUART0(void)
 {
-     // enable uart clock
+    // enable uart clock
 #ifdef MCU_IS_LPC13XX
-    SETBIT(LPC_SYSCON->SYSAHBCLKCTRL,12);    
+    SETBIT(LPC_SYSCON->SYSAHBCLKCTRL,12);
     LPC_SYSCON->USARTCLKDIV = 1;
 #endif
 }
@@ -135,34 +137,34 @@ void initUART0(void)
     
     LPC_UART0->FCR = 0x7;   // enable and reset fifos
     LPC_UART0->LCR = 0x83;	// 8 bits; enable divisor latches
-
+    
     setBaudrate();
     
     LPC_UART0->LCR=3;
-
+    
     LPC_UART0->IER = BIT(0);    // enable receive interrupt
-
+    
     
     NVIC_SetPriority(UART0_IRQn, 0x04);
     NVIC_EnableIRQ(UART0_IRQn); // enable NVIC UART0 Interrupt
-
+    
     uartStream = newInputOutputStream(UART_BUFFER_SIZE);
 }
 
 void sendByteToUART0(UInt8 byte)
 {
 	while( (LPC_UART0->LSR & BIT(5) ) ==0 );
-
+    
 	// test bit-band memory : LSR register is at +0x14 from the base UART memory
-//	while( ( BITBAND( LPC_UART0_BASE + 0x14, 5) )  == 0 );
-
+    //	while( ( BITBAND( LPC_UART0_BASE + 0x14, 5) )  == 0 );
+    
 	LPC_UART0->THR=byte;
 }
 
 void sendBufferToUART0(UInt8 * Buffer,UInt32 Count)
 {
     UInt32 i;
-
+    
     for(i=0;i<Count;i++)
     {
         sendByteToUART0(Buffer[i]);
@@ -186,10 +188,10 @@ void waitDataFromUART0(void)
 }
 
 /*
-UInt32 GetAllBufferFromUART0(UInt8 *buffer, UInt32 len)
-{
-    WaitDataFromStream(uartStream);
-
-    return GetAllBufferFromStream(uartStream,buffer, len);
-}
+ UInt32 GetAllBufferFromUART0(UInt8 *buffer, UInt32 len)
+ {
+ WaitDataFromStream(uartStream);
+ 
+ return GetAllBufferFromStream(uartStream,buffer, len);
+ }
  */
